53bd527e89a18bee4ff375a9e108f25948497bde
Wanchen <wghong@bu.edu>
2024-06-28 18:32:05 -0400

    minor change to ux

--- frontend/src/app/posts/[id]/page.tsx
+++ frontend/src/app/posts/[id]/page.tsx
@@ -1,22 +1,23 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import {
-  Box,
-  Flex,
-  VStack,
-  Image,
-  StackDivider,
-  HStack,
-  Icon,
-  Text,
+	Box,
+	Flex,
+	VStack,
+	Image,
+	StackDivider,
+	HStack,
+	Icon,
+	Text,
+	useToast,
 } from "@chakra-ui/react";
 import { getPost } from "@/utils/postUtils";
 import { getUserPublicInfo } from "@/utils/userUtils";
 import { Post, UserInfo } from "@/lib/model";
 import { PostOwnerInfoCard } from "@/ui/components/users/userInfoComponents";
 import {
-  PostPageSection,
-  CommentSection,
+	PostPageSection,
+	CommentSection,
 } from "@/ui/components/posts/postPageComponents";
 import LoadingWrapper from "@/ui/components/web/LoadingWrapper";
 import { CiHeart } from "react-icons/ci";
@@ -24,114 +25,146 @@ import { VscFlame } from "react-icons/vsc";
 import { TiMessages } from "react-icons/ti";
 
 const PostInfoPage = ({ params }: { params: { id: string } }) => {
-  // post page for the users to upload and delete and view posts they own
-  const [post, setPost] = useState<Post | null>(null);
-  const [user, setUser] = useState<UserInfo | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [hasFetched, setHasFetched] = useState(false);
+	// post page for the users to upload and delete and view posts they own
+	const [post, setPost] = useState<Post | null>(null);
+	const [user, setUser] = useState<UserInfo | null>(null);
+	const [loading, setLoading] = useState(true);
+	const [hasFetched, setHasFetched] = useState(false);
+	const [liked, setLiked] = useState(false);
+	const toast = useToast();
+	const [totalLikes, setTotalLikes] = useState<number>(0);
 
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const postData = await getPost(params.id);
-      setPost(postData);
-      if (postData && postData.postOwnerId) {
-        try {
-          const userInfo = await getUserPublicInfo(postData.postOwnerId);
-          setUser(userInfo);
-        } catch (error) {
-          console.error("Error fetching posts:", error);
-          throw error;
-        } finally {
-          setLoading(false);
-          setHasFetched(true);
-        }
-      }
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-      throw error; 
-    }
-  };
+	const fetchData = async () => {
+		try {
+			setLoading(true);
+			const postData = await getPost(params.id);
+			setPost(postData);
+			if (postData && postData.postOwnerId) {
+				try {
+					setTotalLikes(postData.likes);
+					const userInfo = await getUserPublicInfo(postData.postOwnerId);
+					setUser(userInfo);
+				} catch (error) {
+					console.error("Error fetching posts:", error);
+					throw error;
+				} finally {
+					setLoading(false);
+					setHasFetched(true);
+				}
+			}
+		} catch (error) {
+			console.error("Error fetching posts:", error);
+			throw error;
+		}
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-  return (
-    <>
-      <Box display="flex" justifyContent="center" mt="30" ml="50" mr="50">
-        <LoadingWrapper loading={loading} hasFetched={hasFetched}>
-          <VStack
-            maxW="1500px"
-            minW="60%"
-            maxH="100%"
-            divider={<StackDivider borderColor="gray.200" />}
-            spacing="5"
-            align="stretch"
-          >
-            {/* Hstacks to organize the details of the post */}
-            <HStack spacing={4}>
-              <Flex maxH="400px" align="center" justify="center">
-                <Image
-                  src={post?.imageUrl}
-                  alt="Post Image"
-                  objectFit="cover"
-                  maxW="100%"
-                  height="400px"
-                />
-              </Flex>
+	const handleLike = () => {
+		if (!liked) {
+			setTotalLikes((likes) => likes + 1);
+			setLiked(true);
+			toast({
+				title: "Liked!",
+				status: "success",
+				duration: 2000,
+				isClosable: true,
+			});
+		} else {
+			setTotalLikes((likes) => likes - 1);
+			setLiked(false);
+			toast({
+				title: "Unliked!",
+				status: "info",
+				duration: 2000,
+				isClosable: true,
+			});
+		}
+	};
 
-              {/* column of icons */}
-              <VStack
-                height="100%"
-                minW="50px"
-                maxH="400px"
-                align="center"
-                justify="space-between"
-                direction="column"
-              >
-                <Flex flexDirection="column" align="center">
-                  <Icon as={CiHeart} boxSize="30px" />
-                  <Text fontSize="sm">{post?.likes}</Text>
-                </Flex>
+	return (
+		<>
+			<Box display="flex" justifyContent="center" mt="30" ml="50" mr="50">
+				<LoadingWrapper loading={loading} hasFetched={hasFetched}>
+					<VStack
+						maxW="1500px"
+						minW="60%"
+						maxH="100%"
+						divider={<StackDivider borderColor="gray.200" />}
+						spacing="5"
+						align="stretch"
+					>
+						{/* Hstacks to organize the details of the post */}
+						<HStack spacing={4}>
+							<Flex maxH="400px" align="center" justify="center">
+								<Image
+									src={post?.imageUrl}
+									alt="Post Image"
+									objectFit="cover"
+									maxW="100%"
+									height="400px"
+								/>
+							</Flex>
 
-                <Flex flexDirection="column" align="center">
-                  <Icon as={VscFlame} boxSize="30px" />
-                  <Text fontSize="sm">{post?.views}</Text>
-                </Flex>
+							{/* column of icons */}
+							<VStack
+								height="100%"
+								minW="50px"
+								maxH="400px"
+								align="center"
+								justify="space-between"
+								direction="column"
+							>
+								<Flex
+									flexDirection="column"
+									align="center"
+									cursor="pointer"
+									onClick={handleLike}
+									color={liked ? "red.500" : "gray.400"}
+								>
+									<Icon as={CiHeart} boxSize="30px" />
+									<Text fontSize="sm">{totalLikes}</Text>
+								</Flex>
 
-                <Flex flexDirection="column" align="center">
-                  <Icon as={TiMessages} boxSize="30px" color="blue" />
-                  <Text fontSize="sm">#1</Text>
-                </Flex>
-              </VStack>
+								<Flex flexDirection="column" align="center">
+									<Icon as={VscFlame} boxSize="30px" />
+									<Text fontSize="sm">{post?.views}</Text>
+								</Flex>
 
-              {/* column of comments */}
-              <VStack minW="35%" height="100%" spacing="5" align={"left"}>
-                {user && (
-                  <Flex height="50px">
-                    <PostOwnerInfoCard user={user}></PostOwnerInfoCard>
-                  </Flex>
-                )}
-                <Flex height="calc(100% - 50px)" overflowY="auto">
-                  <CommentSection></CommentSection>
-                </Flex>
-              </VStack>
-            </HStack>
+								<Flex flexDirection="column" align="center">
+									<Icon as={TiMessages} boxSize="30px" color="blue" />
+									<Text fontSize="sm">#1</Text>
+								</Flex>
+							</VStack>
 
-            {post && (
-              <Flex width="100%" minH="100px">
-                <PostPageSection post={post}></PostPageSection>
-              </Flex>
-            )}
+							{/* column of comments */}
+							<VStack minW="35%" height="100%" spacing="5" align={"left"}>
+								{user && (
+									<Flex height="50px">
+										<PostOwnerInfoCard user={user}></PostOwnerInfoCard>
+									</Flex>
+								)}
+								<Flex height="calc(100% - 50px)" overflowY="auto">
+									<CommentSection></CommentSection>
+								</Flex>
+							</VStack>
+						</HStack>
 
-            <Box h="200px" bg="pink.100">
-              For future recommendations
-            </Box>
-          </VStack>
-        </LoadingWrapper>
-      </Box>
-    </>
-  );
+						{post && (
+							<Flex width="100%" minH="100px">
+								<PostPageSection post={post}></PostPageSection>
+							</Flex>
+						)}
+
+						<Box h="200px" bg="pink.100">
+							For future recommendations
+						</Box>
+					</VStack>
+				</LoadingWrapper>
+			</Box>
+		</>
+	);
 };
 export default PostInfoPage;
--- frontend/src/utils/errorUtils.ts
+++ frontend/src/utils/errorUtils.ts
@@ -1,20 +1,27 @@
 import { notFound } from "next/navigation";
 
 const handleResponseStatus = (response: Response, errMsg: string) => {
-  // handle http response, if 401, unauthorized so remove auth token;
-  // if other response that are not ok, throw the corresponding error
-  const { status, ok } = response;
+	// handle http response, if 401, unauthorized so remove auth token;
+	// if other response that are not ok, throw the corresponding error
+	const { status, ok } = response;
 
-  if (status === 401 || status === 403) {
-    localStorage.removeItem("authToken");
-    window.location.href = "/auth";
-    alert("Unauthorized. Please login again.");
-    return;
-  }
+	if (status === 401) {
+		localStorage.removeItem("authToken");
+		window.location.href = "/auth";
+		alert("Unauthorized. Please login again.");
+		return;
+	}
 
-  if (!ok) {
-    throw Error(errMsg);
-  }
+	if (status === 403) {
+		localStorage.removeItem("authToken");
+		window.location.href = "/";
+		alert("Bad Request. Please try again.");
+		return;
+	}
+
+	if (!ok) {
+		throw Error(errMsg);
+	}
 };
 
 export default handleResponseStatus;
